9c14e3b96821aef1d0dee14bb1d9b7ffecc71a9d
Anastasiia <miss.nastenkabuchak@gmail.com>
2024-01-11 15:42:53 +0200

    Solar System

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main (String[] args) {
+        for (SolarSystem planet : SolarSystem.values())  {
+            System.out.println(planet);
+        }
+    }
+
+}
